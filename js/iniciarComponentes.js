54e5bbbfd18eaed10468211522358d748549eea9
kervin <38890576+nirvek225@users.noreply.github.com>
2018-08-18 11:44:30 -0300

    Add files via upload

---
+++ js/iniciarComponentes.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('.scrollspy').scrollSpy(); // ->Para inicializar el scrollSpy
+    $('.slider').slider(); // -> Inicia metodo para generar la libreria de imagenes del home (slider)
+    $(".sidebar-collapse").sideNav(); //->Inicia metodo para generar el menu de amburgesa para moviles
+    $('select').material_select();
+
+});
